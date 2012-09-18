702077fdc26a847a60981bf230438fcfe58f7371
Steve Ridout <steveridout@gmail.com>
2012-09-18 16:22:49 +0200

    Restrict sendFeedback.php:
    - only allow POST requests
    - only allow valid email addresses in fromEmail

--- sendFeedback.php
+++ sendFeedback.php
@@ -1,16 +1,26 @@
 <?php
-if (file_exists('feedbackEmail.txt')) {
-	$toEmail = file_get_contents('feedbackEmail.txt');
 
-	//send email
-	$subject = $_REQUEST['subject'];
-	$message = $_REQUEST['message'];
-	$fromEmail = $_REQUEST['email'];
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+	header("HTTP/1.0 405 Method Not Allowed");
+	die();
+}
 
+if (!file_exists('feedbackEmail.txt')) {
+	echo "No recipient email address. Feedback not sent.";
+	die();
+}
+
+$toEmail = file_get_contents('feedbackEmail.txt');
+$subject = $_REQUEST['subject'];
+$message = $_REQUEST['message'];
+$fromEmail = $_REQUEST['email'];
+
+if ($fromEmail == "" || filter_var($fromEmail, FILTER_VALIDATE_EMAIL) == TRUE) {
 	mail($toEmail, $subject, $message, "From:" . $fromEmail);
 	echo "Thanks for your feedback!";
-} else {
-	echo "No recipient email address. Feedback not sent.";
 }
-?>
+else {
+	echo 'Invalid Email Address';
+}
 
+?>
