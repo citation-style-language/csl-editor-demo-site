f1dbb36f92528f1298fb95ca574cb43d1540a889
Steve Ridout <steveridout@gmail.com>
2012-09-25 13:02:32 +0200

    restrict error log entry to 4000 chars. refs #3

--- logError.php
+++ logError.php
@@ -4,8 +4,17 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 	die();
 }
 
+$message = $_REQUEST['message'];
+
+// Restrict message length
+// Note: log_errors_max_len in php.ini can also restrict log length
+$MAX_LOG_LEN = 4000;
+if (strlen($message) > $MAX_LOG_LEN) {
+	$message = substr($message, 0, $MAX_LOG_LEN);
+}
+
 // write to error log on server
-$success = error_log("CSL Editor Client Error: " . $_REQUEST['message']);
+$success = error_log("CSL Editor Client Error: " . $message);
 if ($success == TRUE) {
         echo "success";
 } else {
