0ee8e6a8364542fc79b7bac9bb74e4e3292a993b
Steve Ridout <steveridout@gmail.com>
2012-11-28 18:00:00 +0100

    deploy

--- src/styleUtils.js
+++ src/styleUtils.js
@@ -79,65 +79,73 @@ define(
 			saveButton = dialog.find('#downloadify'),
 			filename,
 			cslCode;
-		
-		dialog.find('#refManagerInstructions').load("../html/fileDialog.html", function () {
-			filename = styleId.replace(/.*\//g, "").replace(/[\\\/:"*?<>| ]+/g, "-") + '.csl';
+	
+		$.ajax({
+			url: CSLEDIT_urlUtils.getResourceUrl("../html/fileDialog.html"),
+			success : function (data) {
+				dialog.find('#refManagerInstructions').html(data);
 
-			if (typeof(comment) !== "undefined") {
-				// add comment to start
-				cslCode = cslCodeOrData.getCslCode("This style was edited with the Visual CSL Editor (" +
-					window.location.href + ")");
-			} else {
-				debug.assertEqual(typeof(cslCodeOrData), "string");
-				cslCode = cslCodeOrData;
-			}
+				filename = styleId.replace(/.*\//g, "").replace(/[\\\/:"*?<>| ]+/g, "-") + '.csl';
 
-			dialog.dialog({
-				minWidth : 750,
-				minHeight : 450,
-				modal : true,
-				open :  function () {
-					dialog.find('#accordion').accordion({});
-					saveButton.find('a').css({
-						color : "blue",
-						"text-decoration" : "underline"
-					});
-
-					saveButton.children().remove();
-
-					saveButton.downloadify({
-						swf : '../external/downloadify/downloadify.swf',
-						downloadImage : '../external/downloadify/download.png',
-						width : 100,
-						height : 30,
-						filename : filename,
-						data : cslCode,
-						transparent : true,
-						onComplete: function () {
-							alert('Your CSL Style Has Been Saved');
-							dialog.dialog('destroy');
-						},
-						onCancel: function () { },
-						onError: function () { alert('Error saving file.'); }
-					});
-
-					// if it failed, show instructions to install flash player
-					if (saveButton.find('object').length === 0) {
-						dialog.find('#refManagerInstructions').css({display: "none"});
-						dialog.find('#installFlash').html(
-							'<h2>Flash Player not found</h2><br/>' + 
-							'<h3>To save to disk, you need to:' +
-							'<ul>' +
-							'<li><a href="http://get.adobe.com/flashplayer/">Install Adobe Flash Player</a></li>' +
-							'<li>Reload this page and try again</li>' + 
-							'</ul></h3>');
-					} else {
-						dialog.find('#refManagerInstructions').css({display: "block"});
-						dialog.find('#installFlash').html('');
-					}
+				if (typeof(comment) !== "undefined") {
+					// add comment to start
+					cslCode = cslCodeOrData.getCslCode("This style was edited with the Visual CSL Editor (" +
+						window.location.href + ")");
+				} else {
+					debug.assertEqual(typeof(cslCodeOrData), "string");
+					cslCode = cslCodeOrData;
 				}
-			});
-		});
+
+				dialog.dialog({
+					minWidth : 750,
+					minHeight : 450,
+					modal : true,
+					open :  function () {
+						dialog.find('#accordion').accordion({});
+						saveButton.find('a').css({
+							color : "blue",
+							"text-decoration" : "underline"
+						});
+
+						saveButton.children().remove();
+
+						saveButton.downloadify({
+							swf : '../external/downloadify/downloadify.swf',
+							downloadImage : '../external/downloadify/download.png',
+							width : 100,
+							height : 30,
+							filename : filename,
+							data : cslCode,
+							transparent : true,
+							onComplete: function () {
+								alert('Your CSL Style Has Been Saved');
+								dialog.dialog('destroy');
+							},
+							onCancel: function () { },
+							onError: function () { alert('Error saving file.'); }
+						});
+
+						// if it failed, show instructions to install flash player
+						if (saveButton.find('object').length === 0) {
+							dialog.find('#refManagerInstructions').css({display: "none"});
+							dialog.find('#installFlash').html(
+								'<h2>Flash Player not found</h2><br/>' + 
+								'<h3>To save to disk, you need to:' +
+								'<ul>' +
+								'<li><a href="http://get.adobe.com/flashplayer/">Install Adobe Flash Player</a></li>' +
+								'<li>Reload this page and try again</li>' + 
+								'</ul></h3>');
+						} else {
+							dialog.find('#refManagerInstructions').css({display: "block"});
+							dialog.find('#installFlash').html('');
+						}
+					}
+				});
+			},
+			error : function (jaXHR, textStatus, errorThrown) {
+				alert("Couldn't fetch page: " + textStatus);
+			}
+		});	
 	};
 
 	return {
